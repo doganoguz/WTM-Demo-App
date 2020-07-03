21b76007011a4ddb669feee64a819d29bc914f3f
doganoguz <info@doganoguz.com>
2020-07-03 13:45:52 +0300

    WTM Denizli

--- app/src/main/java/com/doganoguz/wtmdenizli/MainActivity.java
+++ app/src/main/java/com/doganoguz/wtmdenizli/MainActivity.java
@@ -7,10 +7,8 @@ import android.os.Bundle;
 import com.doganoguz.wtmdenizli.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,7 +16,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
